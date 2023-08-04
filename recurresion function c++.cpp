053f0f51855a72962f69d8a2b491afc93b686b29
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-04 10:37:48 +0000

    Friday, August 4, 2023 at 4:07 PM India Standard Time

--- recurresion function c++.cpp
+++ recurresion function c++.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int rec(int n){
     int fac;
-    
+    if(n>1){
     fac=n*rec(n-1);
-        
-        
-    
+
     return fac;
+    }
+    else{
+        return 0;
+    }
 }
 
 int main(){
