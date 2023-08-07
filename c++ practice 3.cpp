4b53415c83f867ae5244734fa5a06a52f381c997
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-07 13:51:24 +0000

    Monday, August 7, 2023 at 7:21 PM India Standard Time

---
+++ c++ practice 3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    int rev=0;
+    int tempeven,tempodd;
+    int rem;
+    while(n!=0)
+{
+    rem=n%10;
+    n=n/10;
+    if(rem%2==0){
+        tempeven +=rem;
+    }
+    else{
+        tempodd+=n;
+    }
+}
+cout<<"sum of even"<<tempeven<<endl;
+cout<<"sum of odd "<<tempodd<<endl;
+return 0;
+}
