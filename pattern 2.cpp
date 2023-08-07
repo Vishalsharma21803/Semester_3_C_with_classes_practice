773c001eb45b0438fbd5a17559e1fe4b641f7bf9
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-07 17:31:44 +0000

    Monday, August 7, 2023 at 11:01 PM India Standard Time

---
+++ pattern 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    for(int i=n-1;i=1;i--){
+        cout<<" ";
+        for(int j=1;j<=n;j++){
+            cout<<j;
+            cout<<endl;
+            for(int k =1;k<=n;k++){
+                cout<<k;
+                cout<<endl;
+            }
+        }
+    }
+    return 0;
+
+    
+}
