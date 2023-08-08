14acd50d9c46ae9ced99c4a729f3f1500475c21a
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 11:26:24 +0000

    Tuesday, August 8, 2023 at 4:56 PM India Standard Time

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main(){
-    int n = 1234;
-    int first = n / 1000;
-    cout << first << endl;
+    int n;
+    int count = 0;
+    cout << "enter the number" << endl;
+    cin >>n ;
+    for (int i = 1 ; i < n;i++){
+        for (int j = 1; j < i; j++){
+            if (i % j == 0){
+                count ++;
+            }
+        }
+        if (count == 2){
+            cout << i << endl;
+            count = 0;
+        }
+    }
 }
