b5bada8f626f24b011bcc27d3d52c80e065e2771
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 11:43:29 +0000

    Tuesday, August 8, 2023 at 5:13 PM India Standard Time

---
+++ number pattern.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int row = 1;
+  while (row <= n)
+  {
+    // print space
+    int space = n - row;
+    while (space)
+    {
+      cout << " ";
+      space--;
+    }
+    // print 2nd triangle
+    int col = 1;
+    while (col <= row)
+    {
+      cout << col;
+      col++;
+    }
+
+    // print 3rd triangle
+    int start = row - 1;
+    while (start)
+    {
+      cout << start;
+      start--;
+    }
+    cout << endl;
+    row++;
+  }
+}
