8d5e8c11fccfe23e09f5351d403dbc278d23a907
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 17:46:31 +0000

    Tuesday, August 8, 2023 at 11:16 PM India Standard Time

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,15 +2,11 @@
 using namespace std;
 
 int main(){
-    // int n = 123;
-    // int length = 0;
-    // while(n > 0){
-    //     n = n/10;
-    //     length ++;
-    // }
-    // cout << length;
-    int x=10,y=20;
-  if(++x<9 && ++y>20) cout<<"inside if";
-  else cout<<"inside else";
-  cout<<x<<" "<<y; 
+    int n = 123;
+    int length = 0;
+    while(n > 0){
+        n = n/10;
+        length ++;
+    }
+    cout << length;
 }
