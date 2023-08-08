c2c025284feb34349bc9df45d7d50787e43e3fef
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 10:08:14 +0000

    Tuesday, August 8, 2023 at 3:38 PM India Standard Time

---
+++ square of each digit present in the entered number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    int rem,rem2,temp=0,temp2=0,x=0;
+    while(n!=0){
+        rem=n%10;
+        n/=10;
+        temp=temp*10+rem;
+    }
+
+    // cout<<temp<<endl;
+    x=temp;
+    while(x!=0){
+        rem2=x%10;
+        if((rem2*rem2)<10){
+        temp2=temp2*10+rem2*rem2;
+        }
+        else if((rem2*rem2>9)){
+            temp2=temp2*100+rem2*rem2;
+        }
+        x=x/10;
+    }
+    cout<<temp2<<endl;
+    return 0;
+}
