38f80da56ef154bd6815eb30a7e1f26c35372a47
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 11:21:19 +0000

    Tuesday, August 8, 2023 at 4:51 PM India Standard Time

--- for loop.cpp
+++ for loop.cpp
@@ -5,7 +5,7 @@ int main(){
     cout<<"enter number: ";
     cin>>n;
     int count = 0;
-    for(int i=1;i<n;i++){
+    for(int i=1; i <= n;i++){
         for(int j=1 ; j <= i ; j++){
             if(i % j == 0){
                 cout<<i<<" ";
