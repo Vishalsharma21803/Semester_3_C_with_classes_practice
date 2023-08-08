819b8de6bfccfd8b49793e75c17825ff1028e589
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 09:59:13 +0000

    Tuesday, August 8, 2023 at 3:29 PM India Standard Time

--- practice.cpp
+++ practice.cpp
@@ -4,17 +4,19 @@ int main(){
     int n;
     cout<<"enter number: ";
     cin>>n;
-    int rem,rem2,temp=0,temp2=0;
+    int rem,rem2,temp=0,temp2=0,x=0;
     while(n!=0){
         rem=n%10;
         n/=10;
-        temp=temp*10+rem*rem;
+        temp=temp*10+rem;
     }
+
     cout<<temp<<endl;
-    while(temp!=0){
-        rem2=n%10;
-        temp/=10;
-        temp2=temp2*10+rem2;
+    temp=x;
+    while(x!=0){
+        rem2=x%10;
+        x/=10;
+        temp2=temp2*10+rem2*rem2;
     }
     cout<<temp2<<endl;
     return 0;
