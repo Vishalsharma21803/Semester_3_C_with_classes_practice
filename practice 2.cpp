d48bac3750cfd2b5c1c7cc0a502730ce0981c617
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-08 10:20:23 +0000

    Tuesday, August 8, 2023 at 3:50 PM India Standard Time

---
+++ practice 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int rem=0,temp=0;
+    cout<<"enter number: ";
+    cin>>n;
+    
+    for(int i=n;i>0;){
+        rem=i%10;
+        
+        if(i=n || i==1 ){
+            temp+=rem;
+        }
+        
+    }
+    cout<<temp;
+    return 0;
+    
+
+    
+}
