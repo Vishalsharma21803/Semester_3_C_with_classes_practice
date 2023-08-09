2e40d99579ab59a139af048afcebda3342f14b2f
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-09 11:20:00 +0000

    Wednesday, August 9, 2023 at 4:50 PM India Standard Time

--- pattern 4.cpp
+++ pattern 4.cpp
@@ -4,12 +4,13 @@ int main(){
     int n;
     cout<<"enter number: ";
     cin>>n;
-    int p=1,rem,s;
+    int p=1,rem,s,m=0;
     while(n>0){
         rem=n%10;
         n/=10;
         s=rem*rem;
-        p=p*10+s;
+        m=m+s*p;
+        p*=10;
 
     }
     cout<<p;
