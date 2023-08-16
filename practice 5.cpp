ffbfd4118b3c0748c245278da632d09798c526a8
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-16 14:52:53 +0000

    Wednesday, August 16, 2023 at 8:22 PM India Standard Time

--- practice 5.cpp
+++ practice 5.cpp
@@ -8,21 +8,21 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    int count0=0;
     int count1=0;
-    int count2=0;
     for(int j=0;j<n;j++){
         if(arr[j]==0){
-            count1+=1;
+            count0+=1;
         }
         else if (arr[j]==1){
-            count2+=1;
+            count1+=1;
 
         }
     }
-    for(int i=0;i<count1;i++){
-        cout<<"0"<<" ";
+    for(int i=0;i<count0;i++){
+        arr[i]==0;
     }
-    for(int i=0;i<count2;i++){
+    for(int i=count0;i<n;i++){
         cout<<"1"<<" ";
     }
 
