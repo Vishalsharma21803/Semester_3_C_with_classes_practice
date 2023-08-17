2d511aacacfeabc2f87e040890c360311290714b
Vishal Sharma <vishal2003ggg@gmail.com>
2023-08-17 13:57:55 +0000

    Thursday, August 17, 2023 at 7:27 PM India Standard Time

--- character array.cpp
+++ character array.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
+int length(char arr[]){
+    int count=0;
+    for(int i=0;arr[i]!='\0';i++){
+        count+=1;
+    }
+    return count;
+}
 int main(){
     char arr[100];
     cin>>arr;
     
     cout<<arr;
+
+    length(arr);
     return 0;
 }
