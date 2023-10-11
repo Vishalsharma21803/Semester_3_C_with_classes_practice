c9f3b4c757d87532de9f7bc6df47303499cefdeb
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-11 17:31:12 +0000

    Wednesday, October 11, 2023 at 11:01 PM India Standard Time

---
+++ 2nd largest number in array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    int j;
+    for(j=0;j<n;j++){
+        if(arr[j]>max){
+            max=arr[j];
+        }
+        else{
+            continue;
+        }
+    }
+    cout<<max;
+    return 0;
+}
