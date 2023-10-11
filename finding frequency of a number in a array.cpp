bf9bdf37bcc388a943273ffd168532407e0467ce
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-11 17:47:24 +0000

    Wednesday, October 11, 2023 at 11:17 PM India Standard Time

---
+++ finding frequency of a number in a array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={5,6,8,5,3,9,5,7};
+    int count=0;
+    cout<<"enter: ";
+    int n;
+    cin>>n;
+    for(int i=0;i<8;i++){
+        if(arr[i]==n){
+            count+=1;
+        }
+    }
+    cout<<endl;
+    cout<<"frequency"<<count;
+    return 0;
+}
