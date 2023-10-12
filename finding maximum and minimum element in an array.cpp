3d991d71ec9418c21f40425d55ae9c064cef9584
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-12 18:22:51 +0000

    Thursday, October 12, 2023 at 11:52 PM India Standard Time

--- finding maximum and minimum element in an array.cpp
+++ finding maximum and minimum element in an array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={2,7,5,9,4,3,6,77,99};
+    int arr[]={456,7,5,9,4,3,6,77,99};
     int sizeofarray=(sizeof(arr))/sizeof(arr[0]);
     int max=arr[0];
     for(int i=0;i<sizeofarray;i++){
