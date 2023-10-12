1e458524e485a8df2fed762f1e557168d326d910
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-12 18:37:46 +0000

    Friday, October 13, 2023 at 12:07 AM India Standard Time

--- rotating an array.cpp
+++ rotating an array.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int arr[]={4,6,5,9,7,2,8};
     int sizeofarray=(sizeof(arr))/sizeof(arr[0]);
+    cout<<"size : "<<sizeofarray<<endl;
     cout<<"enter: ";
     int n;
     cin>>n;
