a73ea557ee0561899877716240c3c39eb7c95c3a
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-12 18:34:37 +0000

    Friday, October 13, 2023 at 12:04 AM India Standard Time

---
+++ rotating an array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={4,6,5,9,7,2,8};
+    int sizeofarray=(sizeof(arr))/sizeof(arr[0]);
+    cout<<"enter: ";
+    int n;
+    cin>>n;
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=sizeofarray-1;j<0;j--){
+            temp=arr[sizeofarray-1];
+            arr[j]=arr[j-1];
+
+            
+        }
+        arr[0]=temp;
+    }
+    return 0;
+}
