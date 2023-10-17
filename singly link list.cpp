3c27cbf8d21f86d9cf200ff9b782866c02517c7b
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-17 09:27:05 +0000

    Tuesday, October 17, 2023 at 2:57 PM India Standard Time

--- singly link list.cpp
+++ singly link list.cpp
@@ -101,7 +101,7 @@ int main(){
     deletenode(head,1);
     print(head);
     cout<<endl;
-    deletenode(head,8);
+    deletenode(head,11);
     cout<<"head: "<<head->val<<endl;
     cout<<"tail: "<<tail->val<<endl;
     
