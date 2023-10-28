8e1954fae676295998b9ceff8c2d52239fcc88ca
Vishal Sharma <vishal2003ggg@gmail.com>
2023-10-28 13:49:17 +0000

    Saturday, October 28, 2023 at 7:19 PM India Standard Time

---
+++ queue.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class queue{
+    public:
+    int front;
+    int rare;
+    int *arr;
+    int size;
+
+    queue(int size){
+        this->size=size;
+        arr=new int[size];
+        front=-1;
+        rare=-1;
+    }
+
+    void push(int element){
+        if(size-rare>1){
+            rare++;
+            arr[rare]=element;
+
+        }
+        else{
+            cout<<"queue overflow"<<endl;
+        }
+    }
+    void pop(){
+        if(rare>=0){
+            rare--;
+        }
+        else{
+            cout<<"queue underflow"<<endl;
+        }
+    }
+    bool empty(){
+        if(rare==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    // void size(){
+    //     cout<<"Size of the queue is "<<(rare+1)<<endl;
+    // }
+    
+    
+};
+int main(){
+    queue q1(10);
+    q1.push(5);
+    q1.push(3);
+    q1.push(7);
+    q1.pop();
+    //cout<<q1.empty();
+    //cout<<"size: "<<q1.size();
+    return 0;
+}
